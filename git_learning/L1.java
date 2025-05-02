54411177b5ec82c55c976c1264e0bb4f43efd7ae
samruddhi kunale <kunaleashwini3@gmail.com>
2025-05-02 21:59:13 +0530

    L1

--- git_learning/L1.java
+++ git_learning/L1.java
@@ -1,1 +1,10 @@
-    "We are learning Java"
+class L1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
