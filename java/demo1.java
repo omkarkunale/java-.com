0e1d21d0c0b75f28991f252ed6073afe896e89bb
samruddhi kunale <kunaleashwini3@gmail.com>
2025-05-02 13:20:01 +0530

    done

---
+++ java/demo1.java
@@ -0,0 +1,16 @@
+
+public class demo1 {
+    public static void subtract(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(("hello world"));
+        subtract("a-b");
+    }
+
+    private static void subtract(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'subtract'");
+    }
+}
